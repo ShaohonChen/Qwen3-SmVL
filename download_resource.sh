e4462cc71a2cf3ff735af8e490d7f00da50d90f2
ShaohonChen <shaohon_chen@115lab.club>
2025-07-12 23:06:04 +0800

    init project

---
+++ download_resource.sh
@@ -0,0 +1,6 @@
+# 下载模型
+modelscope download --model Qwen/Qwen3-0.6B --local_dir ./model/Qwen3-0.6B
+modelscope download --model HuggingFaceTB/SmolVLM2-256M-Video-Instruct --local_dir ./model/SmolVLM2-256M-Video-Instruct
+
+# 下载数据集
+modelscope download --dataset AI-ModelScope/the_cauldron --local_dir ./data/the_cauldron
